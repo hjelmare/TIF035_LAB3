1e8c89c347f61969ba461d69f26f124f9406c5c3
Karlssonalbinka <albinka@student.chalmers.se>
2015-02-27 16:41:35 +0100

    adsf

--- task2/task2.py
+++ task2/task2.py
@@ -8,6 +8,7 @@ import numpy as np
 from math import *
 from gpaw import GPAW
 from gpaw import PW
+from eam_calculator import get_calc
 
 #mol90 = read('POSCAR_0.9')
 #  calc = GPAW(mode=PW(50), h= 0.2, xc='PBE', nbands=162, kpts=(int(k),int(k),int(k)), txt='mol90.txt')
@@ -20,58 +21,10 @@ A = 10000
 lmbda = 2
 D = 5
 mu = 0.5
-rMax = 6
-dr=0.2
 
-def Psi(rMax, dr):
-  r_c = 6.5
-  matrixSize = rMax/dr  
+p = (A, lmbda, D, 2*mu)
+calc = get_calc(p)
 
-  i = 0
-  psi = np.zeros(matrixSize)
-  for r in range(0, rMax+dr, dr):
-    x = (r-r_c)/3
-    if x<=0:
-      psi[i] = pow(x,4)/(1+pow(x,4))
-      i += 1
-    else:
-      psi[i] = 0
-      i += 1
-  return(psi)
+mol.set_calculator(calc)
 
-def Phi(rMax, dr ,A ,lmbda, psi):
-  matrixSize = rMax/dr  
-  phi = np.zeros(matrixSize)
-  i = 0
-
-  for r in range(0, rMax+dr, dr):
-    phi[i] = A*exp(-lmbda*r)
-    i += 1
-  phi = phi*psi
-  return(phi)
-
-def F(D, rho):
-  f = -D*np.sqrt(rho)
-  return(f)
-
-def Rho(rMax, dr, mu, psi):
-  nbrSteps = rMax/dr
-  r = np.linspace(0, rMax, nbrSteps)
-  rho = np.exp(-2*mu*r)
-  rho = rho*psi
-  return(rho)
-
-#def optimizationFunc(refMol_90, A,lmbda,D,mu):
-#  #test
-#  cutoff = 6.28721
-#  n = 21
-#  rs = np.arrange(0,n)*(cutoff/n)
-#  for
-psi = Psi(rMax, dr)
-phi = Phi(rMax, dr, A, lmbda, psi)
-rho = Rho(rMax, dr, mu, psi)
-f = F(D, rho)
-
-calc = EAM(elements = mol.get_chemical_symbols(), embedded_energy = f, electron_density = rho, phi=phi)
-
-
+def Func
